0cab78d8f4c812d489baa6366f04901f8b558b53
dennisnderitu254 <dknderitu@gmail.com>
2023-04-06 14:07:50 +0300

    Fabric script that creates and distributes an archive to your web servers, using the function deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['34.138.32.248', '3.226.74.205']
+env.hosts = ['54.162.0.15', '54.237.104.124']
 
 
 @runs_once

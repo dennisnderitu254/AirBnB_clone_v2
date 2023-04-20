0740404d69ba19a34707d7104db07ba48a5be4e7
dennisnderitu254 <dknderitu@gmail.com>
2023-04-20 11:54:12 +0300

    script that starts a Flask web application - /c/<text>: display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,5 +17,11 @@ def hbnb():
     return "HBNB"
 
 
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    """return text given"""
+    return "C {}".format(text.replace("_", " "))
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
